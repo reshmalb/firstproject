55a0f2af3212c41407e43bc467f87932d5addb9c
reshmalb <reshma.lb27@gmail.com>
2023-02-13 15:56:36 +0530

    access of listElement  changed

--- domproject/demo.js
+++ domproject/demo.js
@@ -31,3 +31,5 @@ items[2].style.backgroundColor="green"
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
 }
+var it=document.getElementsByTagName('li')
+it[3].style.backgroundColor="orange"
