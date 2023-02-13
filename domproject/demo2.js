2a183ca54bea354b6abee7accf8fde286a1ba12f
reshmalb <reshma.lb27@gmail.com>
2023-02-13 15:26:55 +0530

    styles of listitem changed

--- domproject/demo2.js
+++ domproject/demo2.js
@@ -25,4 +25,9 @@ hdr2.style.borderBottom='solid 3px black';
 
 var additem=document.getElementById("h2id")
 additem.style.color="Green"
-
+//Get elements by classname;
+var items=document.getElementsByClassName("list-group-item")
+items[2].style.backgroundColor="green"
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
