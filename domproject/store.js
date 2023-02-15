8a8603f09cedbf631cba34d65db7aa342cc34a56
reshmalb <reshma.lb27@gmail.com>
2023-02-15 11:50:46 +0530

    details removed from local storage

--- domproject/store.js
+++ domproject/store.js
@@ -1,5 +1,8 @@
 var form=document.getElementById('signIn-form')
 form.addEventListener('submit',dosignIn)
+var details=document.getElementById('details')
+ details.addEventListener('click',removeDetails)
+
 
 function dosignIn(event) {
 
@@ -22,6 +25,12 @@ function dosignIn(event) {
      li.appendChild(phchild)
      li.appendChild(dtchild)
      li.appendChild(tmchild)
+     var btndel=document.createElement('button')
+     btndel.className="btn btn-danger btn-sm  float-right delete"
+     btndel.id='DelDetails'
+     var btnChild=document.createTextNode('Delete')
+     btndel.appendChild(btnChild)
+     li.appendChild(btndel)
 
      var itemList=document.getElementById('details')
      itemList.appendChild(li)
@@ -35,9 +44,19 @@ function dosignIn(event) {
      //storing values considering email as unique id
      localStorage.setItem(inputemail,storageObj_serialized)
      let storage_values=JSON.parse(localStorage.getItem(inputemail))
-     console.log(storage)    
+   
      sessionStorage.setItem(inputName,inputemail)
+  }
+  function removeDetails(e){
+    var inputemail=document.getElementById('email').value;
+    if(e.target.classList.contains('delete')){     
+      if(confirm('Are you sure to delete this user details?')){
+          var li=e.target.parentElement;
+          details.removeChild(li)
+          localStorage.removeItem(inputemail)
+          
+      }
 
-
+  }
 
   }
